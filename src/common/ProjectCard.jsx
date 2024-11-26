ff3a92a41ef0c0e93ad60a01eb14965337b4d040
BenjiBelfort <159414155+BenjiBelfort@users.noreply.github.com>
2024-11-26 14:34:24 +0100

    labels OK

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,20 +1,43 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; 
+import styles from '../Sections/Projects/ProjectsStyles.module.css';
 
-const ProjectCard = ({ src, link, h3, p }) => {
+const ProjectCard = ({ src, link, h3, p, label }) => {
+
+    let labelClass = "";
+    if (label === "projet en ligne") {
+        labelClass = styles.online;
+    } else if (label === "projet d'étude") {
+        labelClass = styles.study;
+    } else if (label === "projet en cours") {
+        labelClass = styles.ongoing;
+    }
+
+    // Si le lien est vide ou non défini, rendre une div au lieu de l'ancre <a>
+    const ContentWrapper = link ? 'a' : 'div';
+
     return (
-        <a href={link} target='_blank'>
-        <img className="hover" src={src} alt={`${h3} logo`} />
+        <ContentWrapper
+            href={link || undefined}
+            target={link ? "_blank" : undefined}
+            rel={link ? "noopener noreferrer" : undefined}
+            className={`${styles.projectCard} ${link ? '' : styles.noLink}`}
+        >
+            <div className={styles.projectImageWrapper}>
+                {label && <div className={`${styles.label} ${labelClass}`}>{label}</div>}
+                <img className={styles.hover} src={src} alt={`${h3} logo`} />
+            </div>
             <h3>{h3}</h3>
             <p>{p}</p>
-        </a>
+        </ContentWrapper>
     );
 };
 
 ProjectCard.propTypes = {
     src: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
+    link: PropTypes.string,  // link peut être vide ou undefined
     h3: PropTypes.string.isRequired,
     p: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
 };
 
-export default ProjectCard
+export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,7 +4,10 @@ import ladb from '../../assets/ladb.webp'
 import bestOff from '../../assets/bestoff.webp'
 import nina from '../../assets/nina.webp'
 import booki from '../../assets/booki.webp'
+import playdrum from '../../assets/playdrum.webp'
 
+
+// label = "projet en ligne" ou "projet d'étude" ou "projet en cours"
 
 const Projects = () => {
     return (
@@ -16,24 +19,35 @@ const Projects = () => {
                     link="https://www.lesaiguillesdubaron.fr/"
                     h3="Les Aiguilles du Baron"
                     p="Site React one page."
+                    label="projet en ligne"
                 />
                 <ProjectCard
                     src={bestOff}
                     link="https://bestoffmusic.fr/"
                     h3="Best Off'"
                     p="Site HTML & CSS."
+                    label="projet en ligne"
                 />
                 <ProjectCard
                     src={nina}
                     link="https://benjibelfort.github.io/Nina-Carducci/"
                     h3="Nina Carducci"
                     p="Site HTML, CSS & JavaScript."
+                    label="projet d'étude"
                 />
                 <ProjectCard
                     src={booki}
                     link="https://benjibelfort.github.io/Booki/"
                     h3="Booki"
                     p="Site HTML & CSS."
+                    label="projet d'étude"
+                />
+                <ProjectCard
+                    src={playdrum}
+                    link=""
+                    h3="Play Drum"
+                    p="Application React."
+                    label="projet en cours"
                 />
             </div>
         </section>
